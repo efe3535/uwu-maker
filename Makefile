e4c45c923ff1466cc09385bbc4dc293cbd2d32a1
efe3535 <a.efe.akyazi@gmail.com>
2022-01-07 21:50:21 +0300

    First commit

---
+++ Makefile
@@ -0,0 +1,20 @@
+prog :=uwu-maker
+
+release :=--release
+target :=release
+
+
+build:
+	@echo "Building for release."
+	cargo build $(release)
+
+install:
+	@echo "Installing, if it fails try make && sudo make install"
+	cp target/$(target)/$(prog) /usr/bin/$(prog)
+
+uninstall:
+	@echo "Uninstalling, sad to say bye..."
+	rm /usr/bin/$(prog)
+
+help:
+	@echo "usage: make $(prog) [debug=1]"
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::env;
+
+fn main() {
+    let letters: [&str; 6] = ["a", "e", "i", "o", "e", "u"];
+    let mut uwud: String = "".to_string();  
+    let mut args: Vec<String> = env::args().collect();
+    let progname: &str = &args.to_vec()[0];
+    args.remove(0);
+    
+    if args.len() >= 1 && args[0] != "--help" {
+        for mut arg in args {
+            if letters.contains(&&arg.chars().last().unwrap().to_string().as_str()) {
+                arg.push_str("wu ");
+                uwud.push_str(arg.as_str());
+            } else {
+                arg.push_str("uwu ");
+                uwud.push_str(arg.as_str());
+            }
+        }
+    } else {
+        println!("usage: {} <string to make uwu>",progname);
+    }
+    
+    println!("{}", uwud);
+}
+
